4b19581079717e9f8589f7a971a51ea08f817397
Alex Vest <alex.vest@storageos.com>
2021-08-16 18:40:12 +0100

    Attempt to make private package work

--- cmd/Apply.go
+++ cmd/Apply.go
@@ -44,6 +44,7 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// ApplyCmd.PersistentFlags().String("foo", "", "A help for foo")
+	ApplyCmd.PersistentFlags().String("f", "", "")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
