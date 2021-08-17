f034158f7d5917f38aed5a9cb421af0890059d7d
Alex Vest <alex.vest@storageos.com>
2021-08-17 09:16:08 +0100

    Add some tests
    Break out functionality into funcs

--- cmd/apply.go
+++ cmd/apply.go
@@ -1,15 +1,19 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	applyv1 "k8s.io/client-go/applyconfigurations/core/v1"
+	metaapplyv1 "k8s.io/client-go/applyconfigurations/meta/v1"
 
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes"
@@ -27,11 +31,13 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		_, err := clientInit()
+		client, err := clientInit()
 		if err != nil {
 			return fmt.Errorf("got an error: %s", err)
 		}
 
+		// kubecuttle apply arg1 -f arg2
+		// This would print arg1
 		fmt.Printf("apply called with args: %s\n", args)
 
 		input, err := cmd.Flags().GetString("file")
@@ -58,29 +64,13 @@ to quickly create a Cobra application.`,
 
 		yamlDecoder := yaml.NewYAMLOrJSONDecoder(io.NopCloser(strings.NewReader(string(fileContents))), 4096)
 
-		pod := &v1.Pod{}
-		err = yamlDecoder.Decode(pod)
+		pods, err := DecodePods(yamlDecoder)
 		if err != nil {
-			return fmt.Errorf("got err unmarshalling input: \n%s\n, to pod", fileContents)
+			return fmt.Errorf("failed to decode input, got err: %w", err)
 		}
+		ApplyPods(client, pods)
 
-		//pretty.Printf("pod: \n%s\n", pod)
-
-		//ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		//defer cancel()
-		//client.CoreV1().Pods(pod.Namespace).Apply(ctx, &applyv1.PodApplyConfiguration{
-		//	metaapplyv1.TypeMetaApplyConfiguration{},
-		//	&metaapplyv1.ObjectMetaApplyConfiguration{},
-		//	&applyv1.PodSpecApplyConfiguration{},
-		//	&applyv1.PodStatusApplyConfiguration{},
-		//}, metav1.ApplyOptions{})
-
-		podApplyConf, err := applyv1.ExtractPod(pod, "kubectl-client-side-apply")
-		if err != nil {
-			return fmt.Errorf("got err extracting pod, %s", err)
-		}
-
-		fmt.Printf("podApplyConfig: %v\n", *podApplyConf)
+		//fmt.Printf("podApplyConfig: %v\n", *podApplyConf)
 
 		fmt.Printf("apply called with flag arguments: %s\n", input)
 
@@ -120,4 +110,47 @@ func clientInit() (*kubernetes.Clientset, error) {
 	}
 
 	return client, nil
+}
+
+// DecodePods decodes yaml -> *v1.Pod.
+func DecodePods(y *yaml.YAMLOrJSONDecoder) ([]*v1.Pod, error) {
+	pods := []*v1.Pod{}
+
+	for {
+		pod := &v1.Pod{}
+		if err := y.Decode(pod); err != nil {
+			// We expect an EOF error when decoding is done,
+			// anything else should count as a function fail.
+			if err.Error() != "EOF" {
+				return nil, err
+			}
+
+			return pods, nil
+		}
+
+		pods = append(pods, pod)
+	}
+}
+
+func ApplyPods(client *kubernetes.Clientset, pods []*v1.Pod) error {
+	for _, pod := range pods {
+		podApplyConf, err := applyv1.ExtractPod(pod, "kubectl-client-side-apply")
+		if err != nil {
+			return fmt.Errorf("got err extracting pod, %s", err)
+		}
+
+		fmt.Printf("%v", *podApplyConf)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		client.CoreV1().Pods(pod.Namespace).Apply(ctx, &applyv1.PodApplyConfiguration{
+			metaapplyv1.TypeMetaApplyConfiguration{},
+			&metaapplyv1.ObjectMetaApplyConfiguration{},
+			&applyv1.PodSpecApplyConfiguration{},
+			&applyv1.PodStatusApplyConfiguration{},
+		}, metav1.ApplyOptions{})
+	}
+
+	return nil
 }
--- cmd/apply_test.go
+++ cmd/apply_test.go
@@ -1,9 +1,15 @@
 package cmd
 
-import "testing"
+import (
+	"io"
+	"strings"
+	"testing"
 
-func TestApplyCmd(t *testing.T) {
-	input := `
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/util/yaml"
+)
+
+var onePod = `
 apiVersion: v1
 kind: Pod
 metadata:
@@ -17,5 +23,60 @@ spec:
     - sleep
     - "1000000"
 `
-	applyCmd.Run(rootCmd, []string{"-f", input})
+
+var twoPods = `
+apiVersion: v1
+kind: Pod
+metadata:
+  name: busybox-sleep
+  namespace: sre-test
+spec:
+  containers:
+  - name: busybox
+    image: busybox
+    args:
+    - sleep
+    - "1000000"
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: busybox-sleep-less
+  namespace: sre-test
+spec:
+  containers:
+  - name: busybox
+    image: busybox
+    args:
+    - sleep
+    - "1000"
+`
+
+func TestApplyCmd(t *testing.T) {
+
+	//applyCmd.Run(rootCmd, []string{"-f", input})
+}
+
+func TestDecodePod(t *testing.T) {
+	yamlDecoder := yaml.NewYAMLOrJSONDecoder(io.NopCloser(strings.NewReader(onePod)), 4096)
+
+	pods, err := DecodePods(yamlDecoder)
+	require.NoError(t, err, "got error from DecodePods")
+	require.Len(t, pods, 1)
+
+}
+func TestDecodePods(t *testing.T) {
+	yamlDecoder := yaml.NewYAMLOrJSONDecoder(io.NopCloser(strings.NewReader(twoPods)), 4096)
+
+	pods, err := DecodePods(yamlDecoder)
+	require.NoError(t, err, "got error from DecodePods")
+	require.Len(t, pods, 2)
+}
+
+func TestApplyPods(t *testing.T) {
+	yamlDecoder := yaml.NewYAMLOrJSONDecoder(io.NopCloser(strings.NewReader(twoPods)), 4096)
+	pods, err := DecodePods(yamlDecoder)
+	require.NoError(t, err, "got err from decode pods")
+
+	ApplyPods(nil, pods)
 }
