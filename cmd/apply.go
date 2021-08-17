9f41df2bad95bd08673686cae23e4718f4028d86
Alex Vest <alex.vest@storageos.com>
2021-08-17 21:06:19 +0100

    Have create working
    Apply seems to be missing object meta that may be caused by ExtractPod

--- cmd/apply.go
+++ cmd/apply.go
@@ -11,13 +11,17 @@ import (
 
 	"github.com/spf13/cobra"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	applyv1 "k8s.io/client-go/applyconfigurations/core/v1"
-	metaapplyv1 "k8s.io/client-go/applyconfigurations/meta/v1"
 
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	fieldManager string = "kubecuttle"
 )
 
 // applyCmd represents the Apply command
@@ -50,6 +54,7 @@ to quickly create a Cobra application.`,
 			file         string
 		)
 
+		// Parse input
 		switch input {
 		case "-":
 			file = "/dev/stdin"
@@ -62,15 +67,19 @@ to quickly create a Cobra application.`,
 			return fmt.Errorf("failed to read file: %s, got err: %s", file, err)
 		}
 
+		// Attempt to decode input into pods.
 		yamlDecoder := yaml.NewYAMLOrJSONDecoder(io.NopCloser(strings.NewReader(string(fileContents))), 4096)
 
 		pods, err := DecodePods(yamlDecoder)
 		if err != nil {
 			return fmt.Errorf("failed to decode input, got err: %w", err)
 		}
-		ApplyPods(client, pods)
 
-		//fmt.Printf("podApplyConfig: %v\n", *podApplyConf)
+		for _, pod := range pods {
+			if err := CreateOrApplyPod(client, pod); err != nil {
+				fmt.Printf("got err creating or applying pods, err: %s", err)
+			}
+		}
 
 		fmt.Printf("apply called with flag arguments: %s\n", input)
 
@@ -132,25 +141,62 @@ func DecodePods(y *yaml.YAMLOrJSONDecoder) ([]*v1.Pod, error) {
 	}
 }
 
-func ApplyPods(client *kubernetes.Clientset, pods []*v1.Pod) error {
-	for _, pod := range pods {
-		podApplyConf, err := applyv1.ExtractPod(pod, "kubectl-client-side-apply")
-		if err != nil {
-			return fmt.Errorf("got err extracting pod, %s", err)
+func CreateOrApplyPod(client *kubernetes.Clientset, pod *v1.Pod) error {
+	// Attempt to get pods. If pods cannot be gotten then create them.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	p, err := client.CoreV1().Pods(pod.Namespace).Get(ctx, pod.Name, metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Set Managed Field to be our CLI App
+			pod.SetManagedFields([]metav1.ManagedFieldsEntry{
+				{
+					Manager:   fieldManager,
+					Operation: metav1.ManagedFieldsOperationApply,
+					//FieldsV1: &metav1.FieldsV1{
+					//	Raw: []byte("f:metadata"),
+					//},
+				},
+			})
+			// TODO Call CreatePod func
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			pod, err := client.CoreV1().Pods(pod.Namespace).Create(ctx, pod, metav1.CreateOptions{})
+			if err != nil {
+				return err
+			}
+			fmt.Printf("pod %s/%s created", pod.Namespace, pod.Name)
+			return nil
+		} else {
+			return err
 		}
-
-		fmt.Printf("%v", *podApplyConf)
-
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		client.CoreV1().Pods(pod.Namespace).Apply(ctx, &applyv1.PodApplyConfiguration{
-			metaapplyv1.TypeMetaApplyConfiguration{},
-			&metaapplyv1.ObjectMetaApplyConfiguration{},
-			&applyv1.PodSpecApplyConfiguration{},
-			&applyv1.PodStatusApplyConfiguration{},
-		}, metav1.ApplyOptions{})
 	}
+
+	// Call ApplyPod func
+	return ApplyPod(client, p)
+
+}
+
+func ApplyPod(client *kubernetes.Clientset, pod *v1.Pod) error {
+	podApplyConf, err := applyv1.ExtractPod(pod, fieldManager)
+	if err != nil {
+		return fmt.Errorf("got err extracting pod, err: %w", err)
+	}
+
+	fmt.Printf("%v", *podApplyConf)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	appliedPod, err := client.CoreV1().Pods(pod.Namespace).Apply(ctx, podApplyConf, metav1.ApplyOptions{
+		FieldManager: fieldManager,
+	})
+
+	if err != nil {
+		return fmt.Errorf("got err applying pod, err: %w", err)
+	}
+
+	fmt.Printf("applied pod: \n%v", appliedPod)
 
 	return nil
 }
--- cmd/apply_test.go
+++ cmd/apply_test.go
@@ -1,11 +1,14 @@
 package cmd
 
 import (
+	"context"
 	"io"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
@@ -15,6 +18,23 @@ kind: Pod
 metadata:
   name: busybox-sleep
   namespace: sre-test
+spec:
+  containers:
+  - name: busybox
+    image: busybox
+    args:
+    - sleep
+    - "1000000"
+`
+
+var onePodUpdate = `
+apiVersion: v1
+kind: Pod
+metadata:
+  name: busybox-sleep
+  namespace: sre-test
+  labels:
+    foo: bar
 spec:
   containers:
   - name: busybox
@@ -73,10 +93,39 @@ func TestDecodePods(t *testing.T) {
 	require.Len(t, pods, 2)
 }
 
+func TestCreateOrApplyPods(t *testing.T) {
+	client, err := clientInit()
+	require.NoError(t, err, "failed to initialize client")
+
+	yamlDecoder := yaml.NewYAMLOrJSONDecoder(io.NopCloser(strings.NewReader(onePod)), 4096)
+	pods, err := DecodePods(yamlDecoder)
+	require.NoError(t, err, "got error from DecodePods")
+
+	t.Cleanup(func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		err := client.CoreV1().Pods(pods[0].Namespace).Delete(ctx, pods[0].Name, *v1.NewDeleteOptions(0))
+		require.NoErrorf(t, err, "failed to delete pod: %s/%s", pods[0].Namespace, pods[0].Name)
+	})
+
+	err = CreateOrApplyPod(client, pods[0])
+	require.NoError(t, err, "failed to create or apply pods")
+
+	yamlDecoder = yaml.NewYAMLOrJSONDecoder(io.NopCloser(strings.NewReader(onePodUpdate)), 4096)
+	updatedPods, err := DecodePods(yamlDecoder)
+	require.NoError(t, err, "got error from DecodePods")
+
+	err = CreateOrApplyPod(client, updatedPods[0])
+	require.NoError(t, err, "failed to create or apply pods")
+
+}
+
 func TestApplyPods(t *testing.T) {
 	yamlDecoder := yaml.NewYAMLOrJSONDecoder(io.NopCloser(strings.NewReader(twoPods)), 4096)
 	pods, err := DecodePods(yamlDecoder)
 	require.NoError(t, err, "got err from decode pods")
 
-	ApplyPods(nil, pods)
+	for _, pod := range pods {
+		ApplyPod(nil, pod)
+	}
 }
